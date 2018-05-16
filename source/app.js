2d7680d220cad4de69c383469c484e16e9e2428c
Cloud User <ec2-user@ip-10-0-0-101.ec2.internal>
2018-05-16 10:24:16 +0000

    basic react stateful component

--- source/app.js
+++ source/app.js
@@ -2,10 +2,43 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 class ReactClass extends Component {
+  state = {
+    isHeaderHidden: false,
+  };
+
+  handleClick = ()=>{
+    this.setState(prevState =>({
+      isHeaderHidden: !prevState.isHeaderHidden
+    }));
+  }
+
   render() {
+    const {
+      isHeaderHidden
+    } = this.state;
+
+    if(isHeaderHidden){
+      return (
+        <button
+          className="btn btn-default"
+          onClick={this.handleClick}
+        >
+        Toggle Header
+        </button>
+      );
+    }
+
     return(
-      <h1 className="header">React Component</h1>
-    );
+      <div>
+        <h1 className="header">Stateful React Component</h1>
+        <button
+          className="btn btn-default"
+          onClick={this.handleClick}
+        >
+        Toggle Header
+        </button>
+      </div>
+    )
   }
 }
 
